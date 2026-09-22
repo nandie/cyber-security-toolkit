559178c3a288266704d831f7f00e3203b29d3dc9
Nandipha <namakcpt025@student.wethinkcode.co.za>
2026-09-22 16:07:08 +0200

    Add denylist check to PasswordAnalyzer for common weak passwords (closes #3)

--- src/main/java/za/co/nandipha/cybersecurity/service/PasswordAnalyzer.java
+++ src/main/java/za/co/nandipha/cybersecurity/service/PasswordAnalyzer.java
@@ -1,11 +1,24 @@
 package za.co.nandipha.cybersecurity.service;
 
+import java.util.Set;
+
 public class PasswordAnalyzer {
+
+    private static final Set<String> COMMON_WEAK_PASSWORDS = Set.of(
+            "password", "password1", "password1!", "123456", "12345678",
+            "qwerty", "qwerty123", "letmein", "welcome", "admin",
+            "iloveyou", "monkey", "dragon", "football", "baseball",
+            "abc123", "111111", "123123", "sunshine", "master"
+    );
 
     public int checkStrength(String password) {
 
         if (password == null || password.length() < 4) {
             return 0;
+        }
+
+        if (COMMON_WEAK_PASSWORDS.contains(password.toLowerCase())) {
+            return 1;
         }
 
         int score = 0;
